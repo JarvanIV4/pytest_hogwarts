3b260f3a67e375f9e420acccf1a27d708796397a
JarvanIV <2268035948@qq.com>
2020-01-16 23:25:30 +0800

    .

---
+++ 界面+接口自动化练习/lib/operation/interface/index_query.py
@@ -0,0 +1,3 @@
+class IndexQuery():
+
+    def index_query(self, benchmark_code="", benchmark_name="", data_start_dt="", data_end_dt=""):
--- 界面+接口自动化练习/lib/operation/interfaceOps.py
+++ 界面+接口自动化练习/lib/operation/interfaceOps.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from common.interface.HttpAPI import HttpAPI
-from config.interface import queryMobileConfig as qmc
+from config.interface import query_mobile_conf as qmc
+import re
 
 
 class queryInfo(HttpAPI):
@@ -16,5 +17,11 @@ class queryInfo(HttpAPI):
             login_resp = self.send(qmc.api_data, qmc.setting)  # 发送登录接口请求
 
     def get_loginCookie(self, user):
-        self.login_sys_api(user)
+        login_resp =self.login_sys_api(user)
+        cookie = login_resp.header["Set-Cookie"]
+        JSESSIONID = (re.findall("JSESSIONID=(.*); Path", cookie))[0]
+        cs_session_cookie = (re.findall("cs_session_cookie=(.*); path", cookie))[0]
+        cookie = "JSESSIONID=" + JSESSIONID + ";" + "cs_session_cookie=" + cs_session_cookie + ";" + \
+                 "BIGipServerPL_CSST_80=RD501O000000000FFFF370F36CCO80; loginType=local; lang=zh_CH; loginUserId=100301"
+        return cookie
 
