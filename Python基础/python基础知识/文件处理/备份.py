b5f4811c0988ec2eff9fb9b258b107d3dfa218cd
Wind <2268035948@qq.com>
2020-06-21 21:14:56 +0800

    备份

---
+++ Python基础/python基础知识/文件处理/备份.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/21
+# @Author : Wind
+# 备份文件
+from datetime import datetime
+import shutil
+
+today = datetime.today().strftime("%Y-%m-%d")
+copy_dir = 'D:\Program Data\python_work\python_practice'
+backup_dir = 'E:\\我的文档\\备份资料\\python源码\\' + today
+shutil.copytree(copy_dir, backup_dir)
+print(today + "备份成功！")
