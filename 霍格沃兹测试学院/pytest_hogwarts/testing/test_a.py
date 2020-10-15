561904bed330e4701058b3ba70fe25ae061a30b3
Wind <2268035948@qq.com>
2020-10-15 21:23:38 +0800

    测试上传GitHub

---
+++ 霍格沃兹测试学院/pytest_hogwarts/testing/test_a.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# __author__: Wind
+# 2020/10/15 20:20
+
+class
+def test_add(number1, number2):
+    sum = number1 + number2
+    return sum
+
+def test_div(number1, number2):
+    sum = number1 - number2
+    return sum
