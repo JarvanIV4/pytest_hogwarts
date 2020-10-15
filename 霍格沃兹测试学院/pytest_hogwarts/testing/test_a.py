bc40d138fb474f22ceb7bc4829e25081f35c2315
Wind <2268035948@qq.com>
2020-10-15 21:23:38 +0800

    测试上传GitHub

---
+++ 霍格沃兹测试学院/pytest_hogwarts/testing/test_a.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# __author__: Wind
+# 2020/10/15 20:20
+import pytest
+
+
+class Add:
+
+    def test_add(number1, number2):
+        sum = number1 + number2
+        return sum
+
+    # @pytest.mark.parametrize
+    def test_div(number1, number2):
+        sum = number1 - number2
+        return sum
+
+
+class run_test:
+    def setup(self):
+        self.a = Add()
+
+    def
+
+        print(self.test_add(1, 2))
