37d4bae23c030480620897583f9f5dd69463a60c
Wind <2268035948@qq.com>
2020-11-10 23:06:13 +0800

    上传码云

--- 霍格沃兹测试学院/python_test/test_demo1/test_demo1.py
+++ 霍格沃兹测试学院/python_test/test_demo1/test_demo1.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 # __author__: Wind
 # 2020/11/4 20:24
+import pytest
+import yaml
+
+
+class TestData:
+
+    @pytest.mark.parametrize("env", yaml.safe_load(open("./env.yaml")))
+    def test_yaml(self, env):
+        print("测试环境地址是：" + env["dev"])
